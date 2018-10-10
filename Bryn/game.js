9dcd23f21e2b6993a85bd349990dc2a9d9849c51
Christopher L. Miller <cmiller@crossoft.com>
2018-10-10 10:25:17 -0400

    Code cleanup

--- Bryn/game.js
+++ Bryn/game.js
@@ -1,38 +1,37 @@
 var readline = require("readline-sync");
-
 var spaceType = require('./spaceType.js');
 var space = require('./space.js');
 var player = require('./player.js');
 var dict = require("dict");
 
 var players = [];
-
 var board = [];
+
 var spaceTypes = dict({
 	"start" : new spaceType("start", 1, "Brown", "", []),
 	"normal" : new spaceType("normal", 1, "Green", "", []),
 	"elbacko" : new spaceType("elbacko", 3, "Red", "", []),
-
 	"wagon" : new spaceType("wagon", 1, "Green", "", []),
 	"alamo" : new spaceType("alamo", 1, "Grey-Brown", "", []),
 	"end" : new spaceType("end", 3, "Brown", "", [])
-
-
 });
-
-var board = [];
 
 var bank = {
 	inventory : new dict({})
 };
 
 
+/****************************/
 // lay out the board
+/****************************/
+
 for (var i = 0; i <= 77; i++) {
+
 	var newSpace = new space(i);
 	if (i == 0) {
 		newSpace.spaceType = spaceTypes.get("start");
-	} else if (i == 6 || i == 12 || i == 17 || i == 30 || i == 41 || i == 50 || i == 57 || i == 61 || i == 67 || i == 73) {
+	} else if (i == 6 || i == 12 || i == 17 || i == 30 || i == 41 || 
+			i == 50 || i == 57 || i == 61 || i == 67 || i == 73) {
 		newSpace.spaceType = spaceTypes.get("elbacko");
 	} else if (i == 21 || i == 25 || i == 36){
 	    newSpace.spaceType = spaceTypes.get("wagon");
@@ -44,28 +43,37 @@ for (var i = 0; i <= 77; i++) {
 		newSpace.spaceType = spaceTypes.get("normal");
 
 
-console.log("Space " + i + ": " + newSpace.spaceType.name);
+	console.log("Space " + i + ": " + newSpace.spaceType.name);
 	board.push(newSpace);
 }
 
-// create the bank
+/****************************/
+// loading the bank
+/****************************/
 bank.inventory.set("silver", 120);
-
-// create the bank
 bank.inventory.set("gold", 35);
+
+
+/****************************/
+// Ask how many players
+/****************************/
 
 var playerCount = readline.question("How many players?");
 console.log("");
+
+/****************************/
+// Create the players
+//
+
 for (var p = 1; p <= playerCount; p++) {
+
 	var newPlayer = new player(p);
-	newPlayer.inventory.set("gold", 35);
-	newPlayer.inventory.set("silver", 120);
 
-  console.log("Player #" + p);
-  players.push(newPlayer);
+	newPlayer.inventory.set("gold", 1);
+	newPlayer.inventory.set("silver", 5);
 
-// push adds something to an array
-
+	console.log("Player #" + p);
+	// push adds something to an array
+	players.push(newPlayer);
 }
-
 
